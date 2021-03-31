a285364a820e7d1e9c38ad6b5faa2c2d30eacdc0
derciobucuane <dercio.bucuane@fgh.org.mz>
2021-03-31 10:41:21 +0200

    DLT-11 fixed button Fechar and follow dreams UX standard

--- backend/views/servicos-beneficiados/_form.php
+++ backend/views/servicos-beneficiados/_form.php
@@ -181,8 +181,8 @@ if (isset($_REQUEST['ts'])&&($_REQUEST['ts']>0)) {echo Html::activeDropDownList(
 <?php if(isset($_REQUEST['atender']) &&isset($_REQUEST['m'])&&($_REQUEST['m']>0)&&($_REQUEST['atender']==sha1(1)))   {?>
           <?= Html::submitButton($model->isNewRecord ? 'Atender' : 'Actualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     <?php } else { ?>
+       
         <?= Html::submitButton($model->isNewRecord ? 'Salvar' : 'Actualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        <?= Html::a('<i class="glyphicon glyphicon-backward"></i>', ['index'], ['class' => 'btn btn-warning']) ?> 
 <?php }?> 
     </div>
 </div>
