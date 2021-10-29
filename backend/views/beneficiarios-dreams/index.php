077ff674468ea98e09581c7fdf62cc5ef20c35cc
Gerzelio Domingos Saide <gerzeliodomingos@gmail.com>
2021-10-29 01:12:28 -0700

    Melhorias nos filtros de organizacao e distritos

--- backend/views/beneficiarios-dreams/index.php
+++ backend/views/beneficiarios-dreams/index.php
@@ -14,6 +14,7 @@ use app\models\ComiteCirculos;
 use app\models\ComiteCelulas;
 use app\models\Us;
 use app\models\ServicosBeneficiados;
+use app\models\Organizacoes;
 
 use kartik\grid\EditableColumn;
 use app\models\ServicosDream;
@@ -47,7 +48,11 @@ $this->params['breadcrumbs'][] = $this->title;
             'value' => function ($model) {
               return  $model->district_code==NULL ? '-': $model->distrito['district_name'];
             },
-            'filter'=> ArrayHelper::map(Distritos::find()->where(['>','province_code','0'])->orderBy('province_code,district_name ASC')->all(), 'district_code', 'district_name'),
+            'filter'=> ArrayHelper::map(
+                Distritos::find()
+                ->where(['IN','district_code',$dist])
+                ->orderBy('province_code,district_name ASC')->all(),
+                 'district_code', 'district_name'),
           ],       
           
           [
@@ -74,39 +79,12 @@ $this->params['breadcrumbs'][] = $this->title;
             'label'=>'Org',
             'format'=>'html',
             'value'=> function ($model) { return Yii::$app->user->identity->role==20 ? '<small>'.$model->user->parceiro['name'].'</small>':'<small>'.$model->user->parceiro['name'].'</small>'; },
-            'filter'=>array("1"=>"Jhpiego - Sofala","2"=>"FHI - 360 Sofala","3"=>"World Education Inc. - Sofala","4"=>"World Vision - Cidade de Quelimane",
-              "5"=>"FGH - Cidade de Quelimane","6"=>"NWETI - Gaza","8"=>"Associa  o Kugarissica da Munhava - OCB WEI - Beira",
-              "9"=>"NAFEZA - OCB WEI Quelimane","10"=>"ICAP - Nicoadala","12"=>"Associa  o Comussanas - OCB WEI - Beira"
-              ,"13"=>"AMME - OCB WEI Quelimane"
-              ,"14"=>"Kukumbi OCB WEI - Nicoadala"
-              ,"15"=>"World Education Inc. - Gaza"
-              ,"16"=>"World Education Inc. - Zambezia"
-              ,"17"=>"EGPAF - Gaza"
-              ,"18"=>"CDC"
-              ,"19"=>"Udeba-Lab - OCB WEI Limpopo"
-              ,"20"=>"Associação AREPACHO - OCB WEI - Chongoene"
-              ,"21"=>"Associa  o KUVUMBANA - OCB WEI Cidade Xai-Xai"
-              ,"23"=>"Jhpiego - Gaza"
-              ,"24"=>"Associa  o VUKOXA - OCB WEI - Chokwe"
-              ,"25"=>"Associação Comussanas - OCB WEI - Beira"
-              ,"26"=>"Associa  o ACTIVA - OCB WEI - Cidade de Xai-Xai"
-              ,"27"=>"World Vision - Nicoadala"
-              ,"28"=>"Conselho Crist o de Mo ambique - OCB WEI - Beira"
-              ,"29"=>"Direc  o Provincial da Educa  o e Desenvolvimento Humano de Gaza"
-              ,"30"=>"Rede CAME"
-              ,"31"=>"FHI360 COVIDA"
-              ,"32"=>"Jhpiego - Zambezia"
-              ,"33"=>"SDSMAS Namaacha"
-              ,"34"=>"Peace Corps - Corpo da Paz - Chokwe"
-              ,"35"=>"FGH Nicoadala"
-              ,"36"=>"SOPROC - OCB WEI Beira"
-              ,"37"=>"Ministerio da Educa  o Zambezia"
-              ,"38"=>"Associa  o Tiyane Vavassate - OCB FHI360 CoVida - Matutuine"
-              ,"39"=>"ASSEDUCO - OCB FHII360 COVDA Namaacha"
-              ,"40"=>"SDSMAS Matutuine"
-              ,"41"=>"Funda ao ARIEL Glaser"
-              ,"43"=>"N`WETI_Zambezia"
-              ,"44"=>"USAID"),
+            'filter'=>ArrayHelper::map(
+              Organizacoes::find()
+                ->where(['IN','id',$org])
+                ->orderBy('distrito_id ASC')
+                ->all(), 'id', 'name'
+            ),
           ],
           
           [
