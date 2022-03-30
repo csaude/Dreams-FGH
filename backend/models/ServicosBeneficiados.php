aab03933f8779aa7ddeea4b702c4159e498aa795
Hamilton Mutaquiha <hamilton.mutaquiha@gmail.com>
2022-03-30 08:23:48 +0200

    fix query

--- backend/models/ServicosBeneficiados.php
+++ backend/models/ServicosBeneficiados.php
@@ -139,28 +139,7 @@ class ServicosBeneficiados extends \yii\db\ActiveRecord
                 ) s on (s.beneficiario_id = r.beneficiario_id and s.servico_id = r.servico_id)  
                 where r.status_ref = 1 
                 group by r.id
-                having count(s.servico_id)=0
-                
-                union
-                
-                select distinct r.id
-                from 
-                ( 
-                    select r.id, r.beneficiario_id, r.status_ref, rs.servico_id
-                    from app_dream_referencias_s rs 
-                    inner join app_dream_referencias r on rs.referencia_id = r.id 
-                    where r.beneficiario_id = ". $this->beneficiario_id ."
-                ) r 
-                left join 
-                ( 
-                    select beneficiario_id, servico_id 
-                    from app_dream_beneficiario_servicos bs 
-                    where bs.beneficiario_id = ". $this->beneficiario_id ."
-                    and bs.status=0
-                ) s on (s.beneficiario_id = r.beneficiario_id and s.servico_id = r.servico_id)  
-                where r.status_ref = 1 
-                group by r.id
-                having count(s.servico_id)>0;";
+                having count(s.servico_id)=0";
 
         $preparedQuery = Yii::$app->db->createCommand($query);
         $result = $preparedQuery->queryAll();
